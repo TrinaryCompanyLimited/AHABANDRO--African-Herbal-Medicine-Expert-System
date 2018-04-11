40b38d0637d9042ccca249a52dd3271f6728272f
Solomon Yaw Adeklo <sadeklo@st.vvu.edu.gh>
2018-04-11 10:33:45 +0100

    prototype

---
+++ ahabandro/index.php
@@ -0,0 +1,40 @@
+<html>
+<head>
+<title>Ahabandro Expert System</title>
+<link href="https://fonts.googleapis.com/css?family=Cormorant+Infant|Duru+Sans|Imprima|Maitree|Quattrocento+Sans|Quicksand|Revalia" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+
+</head>
+<body>
+
+<nav class="dropdownmenu">
+  <ul>
+    <li><a href="#">Home</a></li>
+    <li><a href="#">About Ahabandro</a></li>
+    <li><a href="#">Services</a>
+      <ul id="submenu">
+        
+        <li><a href="http://www.facebook.com/TrinaryCompanyLiited" target="_blank">Trinary</a></li>
+         </ul>
+    </li>
+    <li><a href="#">Doctors</a></li>
+    <li><a href="#">Contact Us</a></li>
+  </ul>
+</nav><br />
+
+ <div class = "container">
+    <h1 class = "text-center">HERBAL MEDICINE EXPERT SYSTEM FOR STROKE</h1>
+      <div class = "row">
+        <div class = "col-xs-6 col-xs-offset-3">
+          <form>
+           <div class = "form-group">
+              <input name = "title" type = "text" id = "title" class = "form-control" placeholder = "Enter your diagnose symptoms">
+           </div><!---end form-group-->
+
+            <div class = "form-group">
+              <input type = "submit" id = "submit" class = "btn btn-primary col-xs-offset-3 col-xs-6 col-sm-6 col-sm-offset-3" value = "Search">
+            </div><!--end form-group-->
+          </form>
+         
+        </div><!--end col-xs-8-->
+     </div><!--end row-->
